d26ff139c2f97883219db656a5394ab82c22927a
markocopic <41292153+markocopic@users.noreply.github.com>
2019-05-09 23:10:22 +0200

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello col-lg-4 mx-auto">
+      <div>
+        <div class="mt-2">Naziv knjige {{ text }}</div>
+        <b-form-input v-model="knjiga" placeholder="Unesi Naziv knjige"></b-form-input>
+      </div>
+      <div>
+        <div class="mt-2">autor: {{ text }}</div>
+        <b-form-input v-model="autor" placeholder="Unesi Naziv autora"></b-form-input>
+      </div>
+      <div>
+        <div class="mt-2">Izdavac: {{ text }}</div>
+        <b-form-input v-model="izdavac" placeholder="Unesi Naziv izdavaca"></b-form-input>
+      </div>
+      <div>
+        <div class="mt-2">Zanr: {{ text }}</div>
+        <b-form-input v-model="zanr" placeholder="Unesi Naziv zanra"></b-form-input>
+      </div>
+      <b-button variant="success" class="mt-2">Posalji</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+      return {
+        knjiga: '',
+        autor:'',
+        izdavac:'',
+        zanr:''
+      }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about col-lg-8 mx-auto">
+    <b-list-group>
+      <div class="container">
+      <b-list-group-item v-for="(post,i) in posts" :key="i">
+        <div class="row">
+        <div class="col-lg-4" style="my-auto">
+          <img :src="post.thumbnailUrl" alt="post.title" class="my-auto">
+        </div>
+        <div class="col-lg-8 text-left">
+          <h3>{{post.title}}</h3>
+          <p>Autor: Matija Beckovic</p>    
+          <p>Prodavnica: LeoCommerce</p>    
+          <p>Cena: 1200RSD</p>    
+        <b-button variant="danger" class="mt-auto" style="float:right;">Naruci</b-button>  
+        </div>      
+        </div>
+        </b-list-group-item>
+      </div>
+    </b-list-group>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+      return {
+        posts:[]
+      }
+    },
+    mounted(){
+      axios.get('https://jsonplaceholder.typicode.com/photos')
+      .then(response => {
+        for (let i = 0; i < 10; i++) {
+          this.posts.push(response.data[i]);
+        }
+        console.log(this.posts);
+        
+      })
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
